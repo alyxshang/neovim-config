b60009430e32a28796e2e92d6117d46d1e38ad63
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-03-17 23:47:44 +0100

    Formatting.

--- lua/plugins/shelley.lua
+++ lua/plugins/shelley.lua
@@ -1,6 +1,10 @@
+-- Alyx.nvim by Alyx Shang.
+-- Licensed under the FSL v1.
+
+-- Spec for "alyxshang/shelley.nvim".
 return {
-	'alyxshang/shelley.nvim',
-	config = function()
-		require("shelley").setup()
-	end
+    'alyxshang/shelley.nvim',
+    config = function()
+        require("shelley").setup()
+    end
 }
