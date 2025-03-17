1ae590ec1ad0ec9fca96982acd9eebc08cf4d101
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-03-17 21:50:58 +0100

    Update suggest.lua

--- lua/plugins/suggest.lua
+++ lua/plugins/suggest.lua
@@ -3,13 +3,13 @@
 
 -- Spec for "hrsh7th/nvim-cmp".
 return {
-	'hrsh7th/nvim-cmp',
-	dependencies = {
-		'hrsh7th/cmp-nvim-lsp',
-		'saadparwaiz1/cmp_luasnip',
-		'L3MON4D3/LuaSnip'
-	},
-	config = function()
-		require("cmp").setup({})
-	end
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+	'saadparwaiz1/cmp_luasnip',
+	'L3MON4D3/LuaSnip'
+    },
+    config = function()
+        require("cmp").setup({})
+    end
 }
