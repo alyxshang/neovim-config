003453bc7d314d0b88ec8e0644911f9dd200adee
alyxshang <179976644+alyxshang@users.noreply.github.com>
2025-03-17 21:48:07 +0100

    Completions

---
+++ lua/plugins/suggest.lua
@@ -0,0 +1,15 @@
+-- Alyx.nvim by Alyx Shang.
+-- Licensed under the FSL v1.
+
+-- Spec for "hrsh7th/nvim-cmp".
+return {
+	'hrsh7th/nvim-cmp',
+	dependencies = {
+		'hrsh7th/cmp-nvim-lsp',
+		'saadparwaiz1/cmp_luasnip',
+		'L3MON4D3/LuaSnip'
+	},
+	config = function()
+		require("cmp").setup({})
+	end
+}
