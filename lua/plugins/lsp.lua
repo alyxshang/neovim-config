9bbb027b27837a80e91edbfe3b32886b7eed2248
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-03-18 09:06:42 +0100

    Update lsp.lua

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,17 +8,19 @@ return {
         local lspconfig = require('lspconfig')
         lspconfig.rust_analyzer.setup {
             settings = {
+                ['zls'] = {},
+                 ['dartls'] = {},
                 ['rust-analyzer'] = {},
-	        ['ocamllsp'] = {},
-		['clangd'] = {},
-	        ['deno-lsp'] = {
-	            on_attach = on_attach,
-	            root_dir = lspconfig.util.root_pattern(
-	                "deno.json", 
-		        "deno.jsonc"
-	            ),
-	        }
-	    },
+	               ['ocamllsp'] = {},
+		              ['clangd'] = {},
+	               ['deno-lsp'] = {
+	                   on_attach = on_attach,
+	                   root_dir = lspconfig.util.root_pattern(
+	                       "deno.json", 
+		                      "deno.jsonc"
+	                 ),
+	              }
+	          },
         }
     end
 }
