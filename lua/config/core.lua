1b7855dc055ee2f065cc3d7ed529c8ff467989aa
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-03-18 18:10:38 +0100

    Tabs fixed.

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -5,7 +5,9 @@
 local function setupBasics()
     vim.wo.number = true
     vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
+    --vim.opt.tabstop = 2
+    vim.opt.expandtab = true
+    vim.opt.softtabstop = 2
     vim.wo.relativenumber = true
     vim.opt.swapfile = false
     vim.opt.textwidth = 72
