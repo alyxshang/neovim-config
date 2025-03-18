3ce4cee58a8c825d39870ca265a6f138eae64471
🖤𝓐𝓛𝓨𝓧🖤 <179976644+alyxshang@users.noreply.github.com>
2025-03-18 06:42:39 +0100

    Update core.lua

--- lua/config/core.lua
+++ lua/config/core.lua
@@ -6,6 +6,8 @@ local function setupBasics()
     vim.wo.number = true
     vim.opt.shiftwidth = 2
     vim.opt.tabstop = 2
+    vim.wo.relativenumber = true
+    vim.opt.swapfile = false
     vim.opt.textwidth = 72
     vim.opt_local.colorcolumn = "72"
 end
